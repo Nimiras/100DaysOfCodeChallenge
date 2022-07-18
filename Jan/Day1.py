c4655feb54c34752a40973c3e9bc86c71500e6f6
nimiras <pannkinn@gmail.com>
2022-07-18 17:12:17 +0200

    Day15

--- Jan/Day1.py
+++
@@ -1,6 +0,0 @@
-# 1. Input of the city
-city = input("Where were you born?\n")
-# 2. Input for the name of the users's pet
-pet = input("What is the name of your pet?\n")
-# 3. Print out the now generated band-name
-print("You should name your band like " + city + " " + pet + ".")
--- Jan/Day3.py
+++
@@ -1,1 +0,0 @@
-# Da ich schon if und else beherrsche, sehe ich keinen Sinn dieses langwehrige Textadventure zu erstellen.
